5db070b241ab438d06c43caa3f2f9cfce1a7d8be
ruomingxx <ruomingxx@163.com>
2017-08-24 23:09:19 -0400

    Test

--- src/main/java/com/ruomingxx/blog/Application.java
+++ src/main/java/com/ruomingxx/blog/Application.java
@@ -2,14 +2,8 @@ package com.ruomingxx.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Date;
-import java.util.Map;
 
 @SpringBootApplication
-
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/com/ruomingxx/blog/controllers/HomeController.java
+++ src/main/java/com/ruomingxx/blog/controllers/HomeController.java
@@ -1,8 +1,10 @@
 package com.ruomingxx.blog.controllers;
 
+import com.ruomingxx.blog.Constants;
+import com.ruomingxx.blog.models.Post;
 import com.ruomingxx.blog.services.AppSetting;
 import com.ruomingxx.blog.services.PostService;
-import com.ruomingxx.blog.models.Post;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
@@ -40,11 +42,15 @@ public class HomeController {
     }
 
 
-//    @RequestMapping("/")
-//    public String home(Map<String, Object> model) {
-//        model.put("totalPages", new Date());
-//        model.put("posts", "Hello World");
-//        model.put("page", "Hello App");
-//        return "home/index";
-//    }
+    @RequestMapping(value = "about", method = GET)
+    public String about(Model model) {
+        Post post = postService.getPublishedPostByPermalink(Constants.ABOUT_PAGE_PERMALINK);
+
+        if (post == null) {
+            post = postService.createAboutPage();
+        }
+
+        model.addAttribute("about", post);
+        return "home/about";
+    }
 }
